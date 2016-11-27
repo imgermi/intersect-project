fc279a19840b4e65b3308b5e5a80b8b821708cbb
juangermano <juangermano@MacBook-Pro-de-Juan-2.local>
2016-11-27 01:33:34 -0300

    Add existing file

---
+++ header.php
@@ -0,0 +1,16 @@
+<header class="main-nav">
+  <div class="container">
+
+    <a href="#"><img class="logo" src="img/reduced-redicon.png" alt=""></a>
+
+    <nav>
+      <ul>
+        <li><a href="#">PROYECTOS</a></li>
+        <li><a href="#">SERVICIOS</a></li>
+        <li><a href="#">TEAM</a></li>
+        <li><a href="#">CONTACTO</a></li>
+      </ul>
+    </nav>
+
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Intersect Project</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+    <div class="banner">
+      <div class="container">
+        <h1>Creamos intersecciones digitales entre vos y tus clientes</h1>
+      </div>
+    </div>
+  </body>
+</html>
