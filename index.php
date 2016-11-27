e3a7387d3ababe0a53c65e019a324774da7c49b5
juangermano <juangermano@MacBook-Pro-de-Juan-2.local>
2016-11-27 01:48:29 -0300

    Add existing file

--- index.php
+++ index.php
@@ -8,10 +8,55 @@
   </head>
   <body>
     <?php include 'header.php'; ?>
+
+    <!-- BANNER -->
     <div class="banner">
       <div class="container">
         <h1>Creamos intersecciones digitales entre vos y tus clientes</h1>
+        <p>Para superar la competencia tu sitio necesita un  diseño
+          único, un desarrollo detallado y un posicionamiento
+          excepcional.  Somos una agencia creativa que puede
+          ayudarte a generar un producto distinto.</p>
       </div>
     </div>
+
+    <!-- TRABAJOS -->
+    <section class="trabajos">
+      <div class="container">
+
+      </div>
+    </section>
+
+    <!-- SERVICIOS -->
+
+    <section class="servicios">
+      <div class="container">
+
+      </div>
+    </section>
+
+    <!-- TEAM -->
+
+    <section class="team">
+      <div class="container">
+
+      </div>
+    </section>
+
+    <!-- CONTACTO -->
+
+    <section class="contacto">
+      <div class="container">
+
+      </div>
+    </section>
+
+    <!-- FOOTER -->
+    <footer>
+      <div class="container">
+
+      </div>
+    </footer>
+
   </body>
 </html>
