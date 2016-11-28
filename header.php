0faefa1bc5fd57b27142630eb193e2c80dec1c39
juangermano <juangermano@MacBook-Pro-de-Juan-2.local>
2016-11-28 00:49:15 -0300

    Add existing files

--- header.php
+++ header.php
@@ -1,16 +1,17 @@
 <header class="main-nav">
   <div class="container">
 
-    <a href="#"><img class="logo" src="img/reduced-redicon.png" alt=""></a>
+    <a id="logo-click" href="#logo-click"><img class="logo" src="img/reduced-redicon.png" alt=""></a>
 
     <nav>
       <ul>
-        <li><a href="#">PROYECTOS</a></li>
-        <li><a href="#">SERVICIOS</a></li>
-        <li><a href="#">TEAM</a></li>
-        <li><a href="#">CONTACTO</a></li>
+        <li><a id="menu-1" href="#menu-1">PROYECTOS</a></li>
+        <li><a id="menu-2" href="#menu-2">SERVICIOS</a></li>
+        <li><a id="menu-3" href="#menu-3">TEAM</a></li>
+        <li><a id="menu-4" href="#menu-4">CONTACTO</a></li>
       </ul>
     </nav>
 
   </div>
+
 </header>
--- index.php
+++ index.php
@@ -17,12 +17,36 @@
           único, un desarrollo detallado y un posicionamiento
           excepcional.  Somos una agencia creativa que puede
           ayudarte a generar un producto distinto.</p>
+          <img src="img/play-icon.png" alt="">
+          <p>Ver Presentación</p>
       </div>
     </div>
 
     <!-- TRABAJOS -->
     <section class="trabajos">
       <div class="container">
+        <h2>TRABAJOS</h2>
+
+        <article class="nota primer">
+
+        </article>
+        <article class="nota">
+
+        </article>
+
+        <article class="nota primer">
+
+        </article>
+
+        <article class="nota">
+
+        </article>
+
+
+        <!-- CLEANER -->
+        <div class="cleaner"></div>
+
+        <a class="proceso-cta" href="#">VER PROCESO</a>
 
       </div>
     </section>
@@ -31,7 +55,19 @@
 
     <section class="servicios">
       <div class="container">
+        <h2>SERVICIOS</h2>
 
+        <article class="service-note service-margin">
+
+        </article>
+
+        <article class="service-note service-margin">
+
+        </article>
+
+        <article class="service-note">
+
+        </article>
       </div>
     </section>
 
@@ -39,7 +75,48 @@
 
     <section class="team">
       <div class="container">
+        <h2>TEAM</h2>
+        
+        <!-- FIRST FILA -->
+        <article class="team-note team-margin">
 
+        </article>
+
+        <article class="team-note team-margin">
+
+        </article>
+
+        <article class="team-note">
+
+        </article>
+
+
+        <!-- SECOND FILA -->
+        <article class="team-note team-margin">
+
+        </article>
+
+        <article class="team-note team-margin">
+
+        </article>
+
+        <article class="team-note">
+
+        </article>
+
+
+        <!-- THIRD FILA -->
+        <article class="team-note team-margin">
+
+        </article>
+
+        <article class="team-note team-margin">
+
+        </article>
+
+        <article class="team-note">
+
+        </article>
       </div>
     </section>
 
@@ -47,7 +124,15 @@
 
     <section class="contacto">
       <div class="container">
+        <h2>Contanos sobre tu proyecto</h2>
 
+        <form class="contact-form">
+          <input class="campos campos-margin"type="text" name="" value="" placeholder="Nombre">
+          <input class="campos campos-margin"type="email" name="" value="" placeholder="Mail">
+          <input class="campos"type="text" name="" value="" placeholder="Asunto">
+          <textarea placeholder="Escriba su consulta" name="name" rows="8" cols="80"></textarea>
+          <input class="boton" type="submit" name="" value="Enviar">
+        </form>
       </div>
     </section>
 
@@ -57,6 +142,52 @@
 
       </div>
     </footer>
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+    <script type="text/javascript">
+
+    $(document).ready(function (){
+      $("#menu-1").click(function (){
+          $('html, body').animate({
+              scrollTop: $(".trabajos").offset().top
+          }, 500);
+      });
+    });
+
+    $(document).ready(function (){
+      $("#menu-2").click(function (){
+          $('html, body').animate({
+              scrollTop: $(".servicios").offset().top
+          }, 500);
+      });
+    });
+
+    $(document).ready(function (){
+      $("#menu-3").click(function (){
+          $('html, body').animate({
+              scrollTop: $(".team").offset().top
+          }, 500);
+      });
+    });
+
+    $(document).ready(function (){
+      $("#menu-4").click(function (){
+          $('html, body').animate({
+              scrollTop: $(".contacto").offset().top
+          }, 500);
+      });
+    });
+
+    $(document).ready(function (){
+      $("#logo-click").click(function (){
+          $('html, body').animate({
+              scrollTop: $(".banner").offset().top
+          }, 500);
+      });
+    });
+
+    </script>
 
   </body>
 </html>
